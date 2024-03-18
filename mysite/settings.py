dcfb121fec477448dcadc3cb90ede39b2923374e
jha713 <107491242+jha713@users.noreply.github.com>
2024-03-18 18:21:14 +0530

    Update settings.py
    for fetch the data

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,5 +144,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    '*',
+    'https://create-react-app-site-production-edee.up.railway.app',
 ]
