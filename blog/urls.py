525304e92eadcc3e4f09c2925a250baa7685bd85
Abhishek KumarJha <jha.abhishek001@gmail.com>
2024-05-21 14:18:49 +0530

    chnaged to feature flag

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import PostViewSet
+from .views import PostViewSet , feature_flag_view
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet, basename="post")
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('feature-flag/', feature_flag_view, name='feature-flag'),
 ]
