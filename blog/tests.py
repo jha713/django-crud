ea04d80a6e3a308a7d7018308323a7dfece92c8b
Abhishek KumarJha <jha.abhishek001@gmail.com>
2024-05-21 14:18:49 +0530

    chnaged to feature flag

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,80 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import Post
+from unittest.mock import patch
 
-# Create your tests here.
+class PostViewSetTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.post_url = reverse('post-list')
+        self.post_data = {
+            'title': 'Test Title',
+            'content': 'Test Content',
+            'author': 'Test Author'
+        }
+        self.post_instance = Post.objects.create(**self.post_data)
+        self.post_detail_url = reverse('post-detail', kwargs={'pk': self.post_instance.pk})
+
+    @patch('blog.views.is_feature_enabled', return_value=True)
+    def test_list_posts_with_crud_enabled(self, mock_flag):
+        response = self.client.get(self.post_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], self.post_data['title'])
+
+    @patch('blog.views.is_feature_enabled', return_value=False)
+    def test_list_posts_with_crud_disabled(self, mock_flag):
+        response = self.client.get(self.post_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], self.post_data['title'])
+
+    @patch('blog.views.is_feature_enabled', return_value=True)
+    def test_create_post_with_crud_enabled(self, mock_flag):
+        response = self.client.post(self.post_url, self.post_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Post.objects.count(), 2)
+
+    @patch('blog.views.is_feature_enabled', return_value=False)
+    def test_create_post_with_crud_disabled(self, mock_flag):
+        response = self.client.post(self.post_url, self.post_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Post.objects.count(), 1)
+
+    @patch('blog.views.is_feature_enabled', return_value=True)
+    def test_update_post_with_crud_enabled(self, mock_flag):
+        updated_data = {
+            'title': 'Updated Title',
+            'content': 'Updated Content',
+            'author': 'Updated Author'
+        }
+        response = self.client.put(self.post_detail_url, updated_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.post_instance.refresh_from_db()
+        self.assertEqual(self.post_instance.title, updated_data['title'])
+
+    @patch('blog.views.is_feature_enabled', return_value=False)
+    def test_update_post_with_crud_disabled(self, mock_flag):
+        updated_data = {
+            'title': 'Updated Title',
+            'content': 'Updated Content',
+            'author': 'Updated Author'
+        }
+        response = self.client.put(self.post_detail_url, updated_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.post_instance.refresh_from_db()
+        self.assertEqual(self.post_instance.title, self.post_data['title'])
+
+    @patch('blog.views.is_feature_enabled', return_value=True)
+    def test_delete_post_with_crud_enabled(self, mock_flag):
+        response = self.client.delete(self.post_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Post.objects.count(), 0)
+
+    @patch('blog.views.is_feature_enabled', return_value=False)
+    def test_delete_post_with_crud_disabled(self, mock_flag):
+        response = self.client.delete(self.post_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Post.objects.count(), 1)
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,43 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from rest_framework import viewsets
+from featureflags.client import CfClient
+from featureflags.evaluations.auth_target import Target
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 from .models import Post
 from .serializers import PostSerializer
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+api_key = "e478ef59-12a1-468f-aab6-db48b6fd8e90"
+cf = CfClient(api_key)
+cf.wait_for_initialization()
+
+def is_feature_enabled(flag_key, target_identifier, target_name, default=False):
+    target = Target(identifier=target_identifier, name=target_name)
+    return cf.bool_variation(flag_key, target, default)
+
+feature_enabled =is_feature_enabled('crud', 'target1', 'Django_crud', False)
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def create(self, request, *args, **kwargs):
+        if not is_feature_enabled('crud', 'target1', 'Django_crud', False):
+            raise PermissionDenied("CRUD operations are disabled")
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        if not is_feature_enabled('crud', 'target1', 'Django_crud', False):
+            raise PermissionDenied("CRUD operations are disabled")
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        if not is_feature_enabled('crud', 'target1', 'Django_crud', False):
+            raise PermissionDenied("CRUD operations are disabled")
+        return super().destroy(request, *args, **kwargs)
+
+@csrf_exempt
+def feature_flag_view(request):
+    return JsonResponse({'feature_enabled': feature_enabled})
